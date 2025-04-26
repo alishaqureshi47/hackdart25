98b660d0c6cdb45c5dcce7c9712365074fa15dd0
Alejandro Manrique <alejandromanriquenunez@gmail.com>
2025-04-26 13:39:31 -0400

    add: initial setup of google provider

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,32 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { MongoDBAdapter } from "@auth/mongodb-adapter";
+import clientPromise from "@/lib/mongodb";
+import type { NextAuthOptions } from "next-auth";
+
+if (!clientPromise) {
+    throw new Error("Invalid/Missing the mongodb client promise");
+}
+
+export const authOptions: NextAuthOptions = {
+  adapter: MongoDBAdapter(clientPromise, {
+    databaseName: process.env.MONGODB_DB,
+  }),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+  callbacks: {
+    session: async ({ session, user }) => {
+      if (session.user) {
+        session.user.id = user.id;
+      }
+      return session;
+    },
+  },
+};
+
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };

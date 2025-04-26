a64516bd4c270f311da5421f55d626a3da7e2708
Alejandro Manrique <alejandromanriquenunez@gmail.com>
2025-04-26 11:51:19 -0400

    create: initial setup

---
+++ src/shared/components/Sample.tsx
@@ -0,0 +1,12 @@
+// TSX component sample
+import './sample.css'
+import React from 'react';
+
+export const SampleComponent: React.FC = () => {
+    return (
+        <div>
+        <h1>Hello, World!</h1>
+        <p>This is a sample component.</p>
+        </div>
+    );
+}
